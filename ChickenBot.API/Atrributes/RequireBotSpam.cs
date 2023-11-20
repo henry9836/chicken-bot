f97fd8ad408887ba4908255716591f29169072f8
Retekt <90824660+Retekt@users.noreply.github.com>
2023-11-20 18:46:25 +1000

    Implemented RequireBotSpam attribute

---
+++ ChickenBot.API/Atrributes/RequireBotSpam.cs
@@ -0,0 +1,19 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChickenBot.API.Atrributes
+{
+	public class RequireBotSpam : CheckBaseAttribute
+	{
+		public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+		{
+			var configuration = ctx.Services.GetRequiredService<IConfiguration>();
+
+			var botSpamChannel = configuration.GetSection("Channels").GetValue("Botspam", 0ul);
+
+			return Task.FromResult(botSpamChannel == 0 || ctx.Channel.Id == botSpamChannel);
+		}
+	}
+}
