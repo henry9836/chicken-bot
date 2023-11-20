4bf51a9aa04cf45eb32a8f062638b033ed8daf76
Retekt <90824660+Retekt@users.noreply.github.com>
2023-11-20 18:03:43 +1000

    Added info commands module

---
+++ ChickenBot.Info/InfoCommands.cs
@@ -0,0 +1,50 @@
+﻿using ChickenBot.API;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace ChickenBot.Info
+{
+	public class InfoCommands : BaseCommandModule
+	{
+		[Command("avatar"), Hidden]
+		public async Task AvatarCommand(CommandContext ctx)
+		{
+			var embed = new DiscordEmbedBuilder()
+				.WithTitle("Avatar command")
+				.WithDescription("Usage: `avatar [user]`")
+				.WithRequestedBy(ctx.User);
+
+			await ctx.RespondAsync(embed);
+		}
+
+		[Command("avatar"), Description("Displays a user's profile picture")]
+		public async Task AvatarCommand(CommandContext ctx, DiscordMember member)
+		{
+			var embed = new DiscordEmbedBuilder()
+				.WithTitle($"{member.Nickname}'s Avatar")
+				.WithImageUrl(member.AvatarUrl ?? member.DefaultAvatarUrl)
+				.WithRequestedBy(ctx.User);
+
+			await ctx.RespondAsync(embed);
+		}
+
+		[Command("info"), Description("Shows info about the Dsicord server")]
+		public async Task ServerInfoCommand(CommandContext ctx)
+		{
+			if (ctx.Guild == null)
+			{
+				await ctx.RespondAsync("This command cannot be used in DMs");
+				return;
+			}
+
+			var embed = new DiscordEmbedBuilder()
+				.WithTitle($"{ctx.Guild.Name}")
+				.AddField($"Members", $"{ctx.Guild.MemberCount}")
+				.AddField($"Owner", $"<@{ctx.Guild.OwnerId}>")
+				.AddField($"Created", $"<t:{ctx.Guild.JoinedAt.ToUnixTimeSeconds()}:f>");
+
+			await ctx.RespondAsync(embed);
+		}
+	}
+}
