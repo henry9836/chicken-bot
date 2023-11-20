7266a411950dcd336baa3ba1b7b4749e3dc0899c
Retekt <90824660+Retekt@users.noreply.github.com>
2023-11-20 16:28:43 +1000

    Added quote command

---
+++ ChickenBot.API/Atrributes/RequiresVerifiedAttribute.cs
@@ -0,0 +1,29 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChickenBot.API.Atrributes
+{
+	/// <summary>
+	/// Requires that a user has the verified role to run a command
+	/// </summary>
+	public class RequiresVerifiedAttribute : CheckBaseAttribute
+	{
+		public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+		{
+			if (ctx.Member == null)
+			{
+				return Task.FromResult(false);
+			}
+
+			var configuration = ctx.Services.GetRequiredService<IConfiguration>();
+
+			var verifiedRoleID = configuration.GetRequiredSection("Roles").GetValue<ulong>("Verified", 0);
+
+			var isVerified = ctx.Member.Roles.Any(x => x.Id == verifiedRoleID);
+
+			return Task.FromResult(isVerified);
+		}
+	}
+}
---
+++ ChickenBot.Quotes/QuoteCommand.cs
@@ -0,0 +1,81 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ChickenBot.Quotes
+{
+	public class QuoteCommand
+	{
+		public ulong QuotesChannelID => m_Configuration.GetSection("Channels").GetValue("Quotes", 0ul);
+
+		private readonly ILogger<QuoteCommand> m_Logger;
+		private readonly IConfiguration m_Configuration;
+		private readonly string[] m_AllowedTypes = new[] { "png", "jpg", "jpeg" };
+
+		public QuoteCommand(ILogger<QuoteCommand> logger, IConfiguration configuration)
+		{
+			m_Logger = logger;
+			m_Configuration = configuration;
+		}
+
+		public async Task AddQuoteCommand(CommandContext ctx, [RemainingText] string? text)
+		{
+			string? attachmentUrl = null;
+
+			if (ctx.Message.Attachments.Count > 0)
+			{
+				// Quote from uploaded attachment
+				attachmentUrl = ctx.Message.Attachments[0].Url;
+			}
+			else if (text != null)
+			{
+				// Quote from provided image URL
+				var providedUrl = text.Split(' ').First();
+
+				// Ensure it is a http or https URL
+				if (Uri.TryCreate(providedUrl, UriKind.Absolute, out var parsedUri) &&
+					(parsedUri.Scheme == "https" || parsedUri.Scheme == "http"))
+				{
+					attachmentUrl = parsedUri.AbsoluteUri;
+				}
+
+				// Remove link from description
+				text = text.Substring(providedUrl.Length).Trim();
+			}
+
+			if (attachmentUrl == null)
+			{
+				await ctx.RespondAsync("Begawk! You need to attach an image to quote!");
+				return;
+			}
+
+			var extension = Path.GetExtension(attachmentUrl);
+
+			var isAllowed = m_AllowedTypes.Any(x => extension.EndsWith(x, StringComparison.InvariantCultureIgnoreCase));
+
+			if (!isAllowed)
+			{
+				await ctx.RespondAsync("Begawk! You have to attach an image!");
+				return;
+			}
+
+			var quotesChannel = ctx.Guild.GetChannel(QuotesChannelID);
+
+			if (quotesChannel == null)
+			{
+				await ctx.RespondAsync("Begawk! I can't seem to find the quotes channel");
+				return;
+			}
+
+			var embed = new DiscordEmbedBuilder()
+				.WithImageUrl(attachmentUrl)
+				.WithTitle($"Quote by {ctx.Message.Author.Username}")
+				.WithFooter($"<t:{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}:R>")
+				.WithDescription(text);
+
+			m_Logger.LogInformation("Posted quote from user {user}: {url}", ctx.Message.Author.Username, attachmentUrl);
+		}
+	}
+}
