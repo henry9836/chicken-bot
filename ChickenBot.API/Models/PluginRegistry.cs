40924901330e1e224008715056cd5c1c9bbba5d9
Retekt <90824660+Retekt@users.noreply.github.com>
2023-11-22 21:07:00 +1000

    Fixed assembly loader

--- ChickenBot.API/Models/PluginRegistry.cs
+++ ChickenBot.API/Models/PluginRegistry.cs
@@ -7,5 +7,33 @@ namespace ChickenBot.API.Models
 		public List<Assembly> Libraries { get; } = new List<Assembly>();
 		public List<Assembly> Plugins { get; } = new List<Assembly>();
 
+		public Assembly? GetAssembly(string name)
+		{
+			for (int i = 0; i < Plugins.Count; i++)
+			{
+				var plugin = Plugins[i];
+
+				var pluginName = plugin.GetName();
+
+				if (pluginName.Name != null && name.Equals(pluginName.Name, StringComparison.InvariantCultureIgnoreCase))
+				{
+					return plugin;
+				}
+			}
+
+			for (int i = 0; i < Libraries.Count; i++)
+			{
+				var library = Libraries[i];
+
+				var libraryName = library.GetName();
+
+				if (libraryName.Name != null && name.Equals(libraryName.Name, StringComparison.InvariantCultureIgnoreCase))
+				{
+					return library;
+				}
+			}
+
+			return null;
+		}
 	}
 }
--- ChickenBot.Core/Services/AssemblyLoaderService.cs
+++ ChickenBot.Core/Services/AssemblyLoaderService.cs
@@ -26,6 +26,9 @@ namespace ChickenBot.Core.Services
 
 		public async Task StartAsync(CancellationToken cancellationToken)
 		{
+			AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
+
+
 			if (!Directory.Exists(PluginsDirectory))
 			{
 				Directory.CreateDirectory(PluginsDirectory);
@@ -53,6 +56,24 @@ namespace ChickenBot.Core.Services
 			}
 		}
 
+		/// <summary>
+		/// Provides an assembly loader, when the .NET runtime fails to load a library we previously loaded here
+		/// </summary>
+		/// <remarks>
+		/// This allows us to redirect failed assembly load requests to our plugin assembly registry
+		/// </remarks>
+		private Assembly? ResolveAssembly(object? sender, ResolveEventArgs args)
+		{
+			var assemblyName = new AssemblyName(args.Name);
+
+			if (assemblyName.Name != null)
+			{
+				return m_Registry.GetAssembly(assemblyName.Name);
+			}
+			
+			return null;
+		}
+
 		private async Task<Assembly> LoadDLL(string path)
 		{
 			// Load into memory then load from memory, so we don't lock the file
@@ -62,6 +83,8 @@ namespace ChickenBot.Core.Services
 
 		public Task StopAsync(CancellationToken cancellationToken)
 		{
+			AppDomain.CurrentDomain.AssemblyResolve -= ResolveAssembly;
+
 			return Task.CompletedTask;
 		}
 	}
