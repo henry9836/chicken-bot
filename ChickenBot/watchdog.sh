199efa34c5b7684ec6addd0e8cb84f42429e51a1
Henry <henry983615@gmail.com>
2023-11-25 19:16:35 +1300

    Implemented a basic watchdog

---
+++ ChickenBot/watchdog.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+while true
+do
+
+        # Update from git
+        echo
+        echo
+        echo Fetching Changes...
+        echo
+        echo
+        git fetch --all
+        git pull
+
+        # Compile the bot
+        echo
+        echo
+        echo Compiling Bot...
+        echo
+        echo
+        dotnet build ./ChickenBot.sln
+
+        # Move modules into runtime folder
+        echo
+        echo
+        echo Moving modules...
+        echo
+        echo
+        pushd ChickenBot/bin/Debug/net7.0/plugins
+        mv ../../../../../ChickenBot.AdminCommands/bin/Debug/net7.0/ChickenBot.AdminCommands.dll ./
+        mv ../../../../../ChickenBot.API/bin/Debug/net7.0/ChickenBot.API.dll ./
+        mv ../../../../../ChickenBot.AssignableRoles/bin/Debug/net7.0/ChickenBot.AssignableRoles.dll ./
+        mv ../../../../../ChickenBot.ChatAI/bin/Debug/net7.0/ChickenBot.ChatAI.dll ./
+        mv ../../../../../ChickenBot.FlagGame/bin/Debug/net7.0/ChickenBot.FlagGame.dll ./
+        mv ../../../../../ChickenBot.Fun/bin/Debug/net7.0/ChickenBot.Fun.dll ./
+        mv ../../../../../ChickenBot.Info/bin/Debug/net7.0/ChickenBot.Info.dll ./
+        mv ../../../../../ChickenBot.Petitions/bin/Debug/net7.0/ChickenBot.Petitions.dll ./
+        mv ../../../../../ChickenBot.Quotes/bin/Debug/net7.0/ChickenBot.Quotes.dll ./
+        mv ../../../../../ChickenBot.VerificationSystem/bin/Debug/net7.0/ChickenBot.VerificationSystem.dll ./
+        popd
+
+        echo
+        echo
+        echo Starting bot...
+        echo
+        echo
+        pushd ChickenBot/bin/Debug/net7.0/
+        ./ChickenBot
+
+        # Reset
+        popd
+done
