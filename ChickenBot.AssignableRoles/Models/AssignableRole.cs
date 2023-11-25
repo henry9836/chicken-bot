aea4bf6bd46236becd19a3a6eb78f05cb86419e6
Retekt <90824660+Retekt@users.noreply.github.com>
2023-11-25 23:21:04 +1000

    Disabled role comparer

--- ChickenBot.AssignableRoles/Models/AssignableRole.cs
+++ ChickenBot.AssignableRoles/Models/AssignableRole.cs
@@ -6,16 +6,16 @@
 
 		public string RoleName { get; set; } = string.Empty;
 
-		public override bool Equals(object? obj)
-		{
-			return obj is not null
-						&& obj is AssignableRole role
-						&& (role.RoleID == role.RoleID || role.RoleName.Equals(RoleName, StringComparison.InvariantCultureIgnoreCase));
-		}
+		//public override bool Equals(object? obj)
+		//{
+		//	return obj is not null
+		//				&& obj is AssignableRole role
+		//				&& (role.RoleID == role.RoleID || role.RoleName.Equals(RoleName, StringComparison.InvariantCultureIgnoreCase));
+		//}
 
-		public override int GetHashCode()
-		{
-			return HashCode.Combine(RoleID, RoleName.ToLowerInvariant());
-		}
+		//public override int GetHashCode()
+		//{
+		//	return HashCode.Combine(RoleID, RoleName.ToLowerInvariant());
+		//}
 	}
 }
