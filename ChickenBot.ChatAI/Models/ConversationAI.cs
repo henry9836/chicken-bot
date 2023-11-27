cb593fc6f699967357c5ea8fd80aff112fec6609
Henry <henry983615@gmail.com>
2023-11-27 20:50:44 +1300

    made the chicken's brain more random

--- ChickenBot.ChatAI/Models/ConversationAI.cs
+++ ChickenBot.ChatAI/Models/ConversationAI.cs
@@ -151,11 +151,11 @@ namespace ChickenBot.ChatAI.Models
 		/// Determines the next message's temperature
 		/// </summary>
 		/// <remarks>
-		/// Generates a number between 0.85 - 1.4
+		/// Generates a number between 0.9 - 1.4
 		/// </remarks>
 		private double DetermineTemperature()
 		{
-			return 0.85 + (m_Random.NextDouble() * 0.55);
+			return 0.9 + (m_Random.NextDouble() * 0.5);
 		}
 
 		/// <summary>
