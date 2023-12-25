a9879aec818e9effa4e7fcd8b84e571a85058979
Henry Oliver <henry983615@gmail.com>
2023-12-25 16:58:04 +1300

    Add sweatdreams module

---
+++ ChickenBot.SweetDreams/SweetDreamsCommand.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace ChickenBot.SweetDreams;
+
+public class SweetDreamsCommand : BaseCommandModule
+{
+    
+    [Command("sweetdreams"), Description("bedtime")]
+    public async Task SweetDreams(CommandContext ctx)
+    {
+        
+    }
+}
