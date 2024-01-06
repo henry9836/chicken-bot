12ea07f23ff3a31c86a3da91d01140dfffc7f90f
Retekt <90824660+Retekt@users.noreply.github.com>
2024-01-06 13:44:16 +1000

    Added a funny

---
+++ ChickenBot.Fun/Misc/MiscFunCommands.cs
@@ -0,0 +1,46 @@
+﻿using ChickenBot.API.Attributes;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace ChickenBot.Fun.Misc
+{
+	[Hidden]
+	public class MiscFunCommands : BaseCommandModule
+	{
+		private readonly ReactState m_Reacts;
+
+		public MiscFunCommands(ReactState reacts)
+		{
+			m_Reacts = reacts;
+		}
+
+		[Command("PregnantMan"), RequireDirectMessage, RequireBotManager, Hidden]
+		public async Task PregnantManCommand(CommandContext ctx, DiscordUser user)
+		{
+			if (m_Reacts.PregnantManReact.Contains(user.Id))
+			{
+				await ctx.RespondAsync($"{user.Username} is already pregnant.");
+				return;
+			}
+
+			m_Reacts.PregnantManReact.Add(user.Id);
+
+			await ctx.RespondAsync($"Made {user.Username} pregnant.");
+		}
+
+		[Command("Abort"), RequireDirectMessage, RequireBotManager, Hidden]
+		public async Task AbortCommand(CommandContext ctx, DiscordUser user)
+		{
+			if (!m_Reacts.PregnantManReact.Contains(user.Id))
+			{
+				await ctx.RespondAsync($"{user.Username} isn't pregnant.");
+				return;
+			}
+
+			m_Reacts.PregnantManReact.Remove(user.Id);
+
+			await ctx.RespondAsync($"Aborted {user.Username}.");
+		}
+	}
+}
---
+++ ChickenBot.Fun/Misc/ReactService.cs
@@ -0,0 +1,41 @@
+﻿using ChickenBot.API;
+using DSharpPlus;
+using Microsoft.Extensions.Hosting;
+
+namespace ChickenBot.Fun.Misc
+{
+	// Tek: Can I make Norex pregnant?
+	// Tek: I should probably elaborate on that...
+
+	public class ReactService : IHostedService
+	{
+		private readonly DiscordClient m_Discord;
+
+		private readonly ReactState m_Reacts;
+
+		public ReactService(DiscordClient discord, ReactState reacts)
+		{
+			m_Discord = discord;
+			m_Reacts = reacts;
+		}
+
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
+			m_Discord.MessageCreated += OnMessageCreated;
+			return Task.CompletedTask;
+		}
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			m_Discord.MessageCreated -= OnMessageCreated;
+			return Task.CompletedTask;
+		}
+
+		private async Task OnMessageCreated(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs args)
+		{
+			if (m_Reacts.PregnantManReact.Contains(args.Message.Author.Id))
+			{
+				await args.Message.TryReactAsync(m_Discord, ":pregnant_man:");
+			}
+		}
+	}
+}
