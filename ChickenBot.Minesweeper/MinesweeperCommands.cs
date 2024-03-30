199372d282dec27d485a47658f67265a97df7004
Henry Oliver <henry983615@gmail.com>
2024-03-31 01:57:29 +1300

    Implemented a skeleton minesweeper class

---
+++ ChickenBot.Minesweeper/MinesweeperCommands.cs
@@ -0,0 +1,29 @@
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ChickenBot.Minesweeper
+{
+    [Category("Fun")]
+    public class MinesweeperCommands : BaseCommandModule
+    {
+        private readonly IConfiguration m_Configuration;
+        private readonly ILogger<MinesweeperCommands> m_Logger;
+        private readonly Random m_Random;
+        
+        public MinesweeperCommands(IConfiguration configuration, ILogger<MinesweeperCommands> logger)
+        {
+            m_Configuration = configuration;
+            m_Logger = logger;
+            m_Random = new Random();
+        }
+
+        [Command("minesweeper"), Description("Starts a game of minesweeper")]
+        public async Task MinesweeperCommand(CommandContext ctx)
+        {
+            await ctx.RespondAsync("Bang!");
+            return;
+        }
+    }
+}
---
+++ ChickenBot.Minesweeper/MinesweeperService.cs
@@ -0,0 +1,37 @@
+﻿using DSharpPlus;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ChickenBot.Minesweeper
+{
+    public class MinesweeperService : IHostedService
+    {
+        private readonly ILogger<MinesweeperService> m_Logger;
+        private readonly DiscordClient m_Discord;
+        private readonly Random m_Random;
+        
+        public MinesweeperService(ILogger<MinesweeperService> logger, DiscordClient discord)
+        {
+            m_Logger = logger;
+            m_Discord = discord;
+            m_Random = new Random();
+        }
+        
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            m_Discord.MessageCreated += OnMessageCreated;
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            m_Discord.MessageCreated += OnMessageCreated;
+            return Task.CompletedTask;
+        }
+
+        private async Task OnMessageCreated(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs args)
+        {
+            // Implement some logic here :3
+        }
+    }
+}
