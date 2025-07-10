2dee2176d46329c0d54516b0bdc5099728dc26d2
Retekt <90824660+Retekt@users.noreply.github.com>
2025-07-10 17:45:35 +1000

    Update watchdog script for .NET 9

--- watchdog.sh
+++ watchdog.sh
@@ -26,22 +26,22 @@ do
         echo Moving modules...
         echo
         echo
-        pushd ChickenBot/bin/Debug/net8.0/plugins
+        pushd ChickenBot/bin/Debug/net9.0/plugins
         cp ~/config.json.runtime.bak ../config.json
-        mv ../../../../../ChickenBot.AdminCommands/bin/Debug/net8.0/ChickenBot.AdminCommands.dll ./
-        mv ../../../../../ChickenBot.API/bin/Debug/net8.0/ChickenBot.API.dll ./
-        mv ../../../../../ChickenBot.AssignableRoles/bin/Debug/net8.0/ChickenBot.AssignableRoles.dll ./
-        mv ../../../../../ChickenBot.ChatAI/bin/Debug/net8.0/ChickenBot.ChatAI.dll ./
-        mv ../../../../../ChickenBot.FlagGame/bin/Debug/net8.0/ChickenBot.FlagGame.dll ./
-        mv ../../../../../ChickenBot.Fun/bin/Debug/net8.0/ChickenBot.Fun.dll ./
-        mv ../../../../../ChickenBot.Info/bin/Debug/net8.0/ChickenBot.Info.dll ./
-mv ../../../../../ChickenBot.Minesweeper/bin/Debug/net8.0/ChickenBot.Minesweeper.dll ./
-mv ../../../../../ChickenBot.Music/bin/Debug/net8.0/ChickenBot.Music.dll ./
-mv ../../../../../ChickenBot.Petitions/bin/Debug/net8.0/ChickenBot.Petitions.dll ./
-mv ../../../../../ChickenBot.SweetDreams/bin/Debug/net8.0/ChickenBot.SweetDreams.dll ./
-mv ../../../../../ChickenBot.Quotes/bin/Debug/net8.0/ChickenBot.Quotes.dll ./
-mv ../../../../../ChickenBot.ReverseSearch/bin/Debug/net8.0/ChickenBot.ReverseSearch.dll ./
-mv ../../../../../ChickenBot.VerificationSystem/bin/Debug/net8.0/ChickenBot.VerificationSystem.dll ./
+        mv ../../../../../ChickenBot.AdminCommands/bin/Debug/net9.0/ChickenBot.AdminCommands.dll ./
+        mv ../../../../../ChickenBot.API/bin/Debug/net9.0/ChickenBot.API.dll ./
+        mv ../../../../../ChickenBot.AssignableRoles/bin/Debug/net9.0/ChickenBot.AssignableRoles.dll ./
+        mv ../../../../../ChickenBot.ChatAI/bin/Debug/net9.0/ChickenBot.ChatAI.dll ./
+        mv ../../../../../ChickenBot.FlagGame/bin/Debug/net9.0/ChickenBot.FlagGame.dll ./
+        mv ../../../../../ChickenBot.Fun/bin/Debug/net9.0/ChickenBot.Fun.dll ./
+        mv ../../../../../ChickenBot.Info/bin/Debug/net9.0/ChickenBot.Info.dll ./
+mv ../../../../../ChickenBot.Minesweeper/bin/Debug/net9.0/ChickenBot.Minesweeper.dll ./
+mv ../../../../../ChickenBot.Music/bin/Debug/net9.0/ChickenBot.Music.dll ./
+mv ../../../../../ChickenBot.Petitions/bin/Debug/net9.0/ChickenBot.Petitions.dll ./
+mv ../../../../../ChickenBot.SweetDreams/bin/Debug/net9.0/ChickenBot.SweetDreams.dll ./
+mv ../../../../../ChickenBot.Quotes/bin/Debug/net9.0/ChickenBot.Quotes.dll ./
+mv ../../../../../ChickenBot.ReverseSearch/bin/Debug/net9.0/ChickenBot.ReverseSearch.dll ./
+mv ../../../../../ChickenBot.VerificationSystem/bin/Debug/net9.0/ChickenBot.VerificationSystem.dll ./
 popd
 
         echo
@@ -49,7 +49,7 @@ popd
         echo Starting bot...
         echo
         echo
-        pushd ChickenBot/bin/Debug/net8.0/
+        pushd ChickenBot/bin/Debug/net9.0/
         ./ChickenBot
 
         # Backup to tmp
