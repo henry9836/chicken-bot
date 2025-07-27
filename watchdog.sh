fb0fbba2332b76e20a558a6e577a181606ece79e
Henry <37818378+henry9836@users.noreply.github.com>
2025-07-27 16:12:30 +1200

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -5,38 +5,35 @@ export chicken_plugin_config=plugins.cfg
 mkdir -p $HOME
 dotnet restore ./ChickenBot.sln
 
-while true
-do
-        # Update from git
-        echo
-        echo
-        echo Fetching Changes...
-        echo
-        echo
-        git fetch --all
-        git pull
+# Update from git
+echo
+echo
+echo Fetching Changes...
+echo
+echo
+git fetch --all
+git pull
 
-        # Compile the bot
-        echo
-        echo
-        echo Compiling Bot...
-        echo
-        echo
-        dotnet clean ./ChickenBot.sln
-        dotnet build ./ChickenBot.sln
-        
-        # Start the bot
-        echo
-        echo
-        echo Starting bot...
-        echo
-        echo
-        pushd ChickenBot/bin/Debug/net9.0/
-        ./ChickenBot
+# Compile the bot
+echo
+echo
+echo Compiling Bot...
+echo
+echo
+dotnet clean ./ChickenBot.sln
+dotnet build ./ChickenBot.sln
 
-        # Backup to tmp
-        cp config.json ../../../../../config.json.runtime.bak
+# Start the bot
+echo
+echo
+echo Starting bot...
+echo
+echo
+pushd ChickenBot/bin/Debug/net9.0/
+./ChickenBot
 
-        #Reset
-        popd
-done
+# Backup to tmp
+cp config.json ../../../../../config.json.runtime.bak
+
+#Reset
+popd
